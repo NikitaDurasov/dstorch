fc6820e80a5a501bad4becb45ef016cc54aa1edf
Durasov Nikita <yassnda@gmail.com>
2019-02-10 13:32:45 +0300

    Base version of SegmentationBase class

--- segmentation/segmentation_utils.py
+++ segmentation/segmentation_utils.py
@@ -1,18 +1,34 @@
 from torch.utils.data import Dataset
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 
 
-class SegmentationBase(ABC, Dataset):
+class SegmentationBase(Dataset):
 
-    def __init__(self, splits_file, transform):
-        self.splits_file = splits_file
+    def __init__(self, data_path, transform, split):
+        self.data_path = data_path
         self.transform = transform
+        self.split = split
+
+        self.data_files = self.generate_split(data_path)
 
     def __len__(self):
-        pass
+        return len(self.data_files)
 
     def __getitem__(self, index):
-        pass
+        sample = self.data_files[index]
+
+        image_name = sample[0]
+        mask_name = sample[1]
+
+        image = self.load_image(image_name)
+        mask = self.load_segmentation(mask_name)
+
+        sample = {"image": image, "mask": mask}
+
+        if self.transform:
+            sample = self.transform(**sample)
+
+        return sample
 
     @abstractmethod
     def load_image(self, filename):
@@ -20,4 +36,8 @@ class SegmentationBase(ABC, Dataset):
 
     @abstractmethod
     def load_segmentation(self, filename):
+        pass
+
+    @abstractmethod
+    def generate_split(self, data_path):
         pass
