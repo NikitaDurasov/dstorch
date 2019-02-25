b60774a3489411a91c23a55178e3d6e33803979b
Nikita Durasov <yassnda@gmail.com>
2019-02-25 13:00:47 +0300

    hdf5 dataset class for segmentation

--- segmentation/segmentation_utils.py
+++ segmentation/segmentation_utils.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from abc import abstractmethod
+import h5py
 import os
 
 module_path = os.path.dirname(os.path.realpath(__file__))
@@ -76,6 +77,70 @@ class SegmentationBase(Dataset):
             split_path = os.path.join(module_path, "splits", self.name() + "_test_split.txt")
             split_file = open(split_path)
             return list(map(self.line_to_filepaths, split_file.readlines()))
+
+class SegmentationHDF5(Dataset):
+
+    def __init__(self, hdf5_path, split, transform=None):
+        self.data_path = hdf5_path
+        self.transform = transform
+        self.split = split
+
+        hdf5_data = h5py.File(self.data_path, 'r')
+        self.indexes = self.generate_split()
+
+        self.keys = self.generate_keys()
+        self.data = [hdf5_data.get(key) for key in self.keys]
+        self.new_keys = self.generate_new_keys()
+
+    def __len__(self):
+        return len(self.indexes)
+
+    @abstractmethod
+    def generate_keys(self):
+        pass
+
+    @abstractmethod
+    def generate_new_keys(self):
+        pass
+
+    @abstractmethod
+    def process_data(self, sample):
+        pass
+
+    @abstractmethod
+    def name(self):
+        pass
+
+    def __getitem__(self, index):
+        sample = {}
+        idx = self.indexes[index]
+
+        for key, data in zip(self.new_keys, self.data):
+            sample[key] = data[idx]
+
+        sample = self.process_data(sample)
+
+        if self.transform:
+            sample = self.transform(**sample)
+
+        return sample
+
+    def generate_split(self):
+
+        if self.split == 'train':
+            indexes_path = os.path.join(module_path, "splits", self.name() + "_train_split.txt")
+            indexes_file = open(indexes_path)
+            return list(map(int, indexes_file.readlines()))
+
+        if self.split == 'valid':
+            indexes_path = os.path.join(module_path, "splits", self.name() + "_val_split.txt")
+            indexes_file = open(indexes_path)
+            return list(map(int, indexes_file.readlines()))
+
+        if self.split == 'test':
+            indexes_path = os.path.join(module_path, "splits", self.name() + "_test_split.txt")
+            indexes_file = open(indexes_path)
+            return list(map(int, indexes_file.readlines()))
 
 
 
