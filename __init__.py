f90295a3a8a8771469db073eb1623887abf403a6
Nikita Durasov <yassnda@gmail.com>
2019-03-02 02:20:13 +0300

    adding first ziped files for splits and changes in init for aumatic unpack of them

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
 from . import segmentation
 from . import depth
 from . import mixed
+
+import os 
+import sys
+
+# TODO unpack splits to dir in home
+if not os.path.isdir("~/.dstorch_splits"):
+	os.mkdir("~/.dstorch_splits")
